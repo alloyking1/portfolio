83ee3249ddf3a84deace2e381e76cf9b51a4f4e2
alloyking1 <alloyking1@gmail.com>
2020-02-07 19:06:28 -0500

    saving user info to state and manipulation dom

--- resources/js/components/Dashboard.vue
+++ resources/js/components/Dashboard.vue
@@ -11,28 +11,30 @@
 
                     <router-link class="btn btn-icon btn-3 btn-primary" type="button" to="/create/profile">
                         <span class="btn-inner--icon"><i class="ni ni-bag-17"></i></span>
-
                         <span class="btn-inner--text">Create Prfile</span>
-
                     </router-link>
-                    <!-- <div class="col-md-2"></div>
-                    <div class="col-md-10">
-                        <div class="card shadow">
-                            <div class="card-body">
-                                <h4>Welcome {{this.user.name}}</h4>
+                </div>
+
+                <div v-for="profile in user.user_profile">
+                    <div class="row mt-4">
                     
-                                <p>The kit comes with three pre-built pages to help you get started faster. You can change the text and images and you're good to go.</p>
-            
-                                <router-link class="btn btn-icon btn-3 btn-primary" type="button" to="/create/profile">
-                                    <span class="btn-inner--icon"><i class="ni ni-bag-17"></i></span>
-
-                                    <span class="btn-inner--text">Create Prfile</span>
-
-                                </router-link>
+                    
+                        <div class="col-md-4 mt-4">
+                            <div class="card shadow">
+                                <div class="card-body">
+                                    <h4>{{profile.Profession}}</h4>
+                                    <hr>
+                                    <p>{{profile.description}}</p>
+                                    <hr>
+                                    <p>{{profile.location}}</p>
+                                    
+                                    <button type="button" class="btn btn-outline-info">Edit</button>
+                                    <button type="button" class="btn btn-outline-danger">Delete</button>
+                                </div>
                             </div>
                         </div>
+        
                     </div>
-                    <div class="col-md-2"></div> -->
                 </div>
 
             </div>
@@ -49,7 +51,7 @@ import {fetchUser} from '../api/users.api';
 export default {
     data(){
         return {
-            user:{}
+            user:''
         }
     },
 
@@ -57,6 +59,7 @@ export default {
         fetchUser(){
             fetchUser().then(res => {
                 this.user = res.data.user;
+                console.log(res.data.user.user_profile);
                 this.$store.commit('SET_USERS', this.user);
                 // this.$store.dispatch("SET_USER", res.user);
             })
